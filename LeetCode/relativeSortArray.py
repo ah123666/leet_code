69df2b9543a3dee798c5d7d871353d42c183475a
xyym <1920376753@qq.com>
2020-12-12 22:54:38 +0800

    add a file

---
+++ LeetCode/relativeSortArray.py
@@ -0,0 +1,38 @@
+"""
+给你两个数组，arr1 和 arr2，
+
+arr2 中的元素各不相同
+arr2 中的每个元素都出现在 arr1 中
+对 arr1 中的元素进行排序，使 arr1 中项的相对顺序和 arr2 中的相对顺序相同。未在 arr2 中出现过的元素需要按照升序放在 arr1 的末尾。
+
+ 
+
+示例：
+
+输入：arr1 = [2,3,1,3,2,4,6,7,9,2,19], arr2 = [2,1,4,3,9,6]
+输出：[2,2,2,1,4,3,3,9,6,7,19]
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/relative-sort-array
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+from typing import List
+
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        result = []
+        other = []
+        for item in arr2:
+            result.extend([item] * arr1.count(item))
+        for item in arr1:
+            if item not in arr2:
+                other.append(item)
+        other.sort()
+        result.extend(other)
+        return result
+
+
+s = Solution()
+arr1 = [2,3,1,3,2,4,6,7,9,2,19]
+arr2 = [2,1,4,3,9,6]
+print(s.relativeSortArray(arr1, arr2))
